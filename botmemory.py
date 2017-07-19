2fdbdcfc18b716815b176832067fd7afaff50717
Radmirkus <radmirka74@gmail.com>
2017-07-19 06:46:21 +0500

    config fix

--- botmemory.py
+++ botmemory.py
@@ -1,21 +1,23 @@
 import configparser
+import json
 
 
 
+config_path = "melbo.config"
 config = configparser.ConfigParser()
-config.read("melbo.config")
+config.read(config_path)
 
-name = config.get("configuration", "name")
-prefix = config.get("configuration", "prefix")
-version = config.get("configuration", "version")
-chat_list = list(config.get("configuration", "chat_list"))
-ignore_users = list(config.get("configuration", "ignore_users"))
-testmode = bool(config.get("configuration", "testmode"))
-trainmode = bool(config.get("configuration", "trainmode"))
-msg_timeout = int(config.get("configuration", "msg_timeout"))
-msg_wait = int(config.get("configuration", "msg_wait"))
-active_mod_max_time = int(config.get("configuration", "active_mod_max_time"))
-access_token = config.get("configuration", "access_token")
-app_id = config.get("configuration", "app_id")
-my_id = config.get("configuration", "my_id")
-api_version = config.get("configuration", "api_version")
+name = config.get("optional", "name")
+prefix = config.get("optional", "prefix")
+version = config.get("optional", "version")
+chat_list = json.loads(config.get("optional", "chat_list"))
+ignore_users = json.loads(config.get("optional", "ignore_users"))
+testmode = config.getboolean("settings", "testmode")
+trainmode = config.getboolean("settings", "trainmode")
+msg_timeout = config.getint("settings", "msg_timeout")
+msg_wait = config.getint("settings", "msg_wait")
+active_mod_max_time = config.getint("settings", "active_mod_max_time")
+access_token = config.get("private", "access_token")
+app_id = config.get("info", "app_id")
+my_id = config.get("private", "my_id")
+api_version = config.get("info", "api_version")
